43cc9401e79902d4d1d31cf437e7f90c38e9db23
Matheus Andrade De Araujo <113406654+MatheusAraujo86@users.noreply.github.com>
2023-07-21 14:38:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// Selecionando os elementos HTML
 const asideSection = document.querySelector("aside");
 const mainSection = document.querySelector("main");
 
 const signInBtn = document.getElementById("login-btn");
 const signUpBtn = document.getElementById("register-btn");
 
+// Adicionando um evento de clique ao botão
 signInBtn.addEventListener("click", () => {
   mainSection.classList.add("slide-right");
   asideSection.classList.add("slide-left");
 });
 
+// Adicionando um evento de clique ao botão de registro.
 signUpBtn.addEventListener("click", () => {
   mainSection.classList.remove("slide-right");
   asideSection.classList.remove("slide-left");
